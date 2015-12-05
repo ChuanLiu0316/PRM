7180c77cd95820653dbf25bc22e80713de3ebce4
George Liu <liuchuan95@gmail.com>
2015-12-05 16:09:39 -0500

    roughly finished A* algo, need to integrate

---
+++ A*.py
@@ -0,0 +1,40 @@
+from Queue import PriorityQueue
+class Astar(object):
+    @staticmethod
+    def run(graph, start, end, outfile):
+        resultRoad = []
+        fo = open(outfile, "w")
+        def e_dis((cx,cy),(endx, endy)):
+            return math.sqrt((endx-cx)*(endx-cx) + (endy-cy)*(endy-cy))
+
+        def recursive_print(currentS):
+            if currentS[1] == 0 :
+                return
+            else:
+                recursive_print(currentS[3])
+                fo.write(str(currentS[4]))  
+        pq = PriorityQueue()
+        startS = (e_dis(start,end), 0.0, start, None)
+        pq.put(startS);
+        popNumber = 0 
+        while not pq.empty() : 
+            currentS = pq.get()
+            popNumber = popNumber+1
+            (index, (cx,cy)) = currentS[2]
+            cost = currentS[1]
+            if (cx,cy) == end: 
+                fo.write(str(popNumber))
+                fo.write("\n")
+                recursive_print(currentS)
+                fo.close()
+                return 0    
+            else: 
+                for neighbor in graph[currentS[0]]:     
+                    h = e_dis(neighbor[1], end)
+                    newcost = cost+e_dis(neighbor[1], (cx,cy))
+                    f = h+g
+                    pq.put((f, newcost, neighbor[1],currentS[2]))
+  
+        #this shouldn't happen    
+        return -1    
+
