c5b58636b6ee506413f1e994e553d61d752dd26d
George Liu <liuchuan95@gmail.com>
2015-12-05 22:24:06 -0500

    fix nasty bug in isWayBlcoked function

--- prm.py
+++ prm.py
@@ -18,16 +18,65 @@ class PRM(object):
     # isWayBlocked, given two point in the Image, check if the line between 
     # the two point is blocked by obstacles  
     def isWayBlocked(self, (x1, y1), (x2, y2)): 
+        def outOfIndex(x,y):
+            height = len(self.TwoDMatrix)
+            width = len(self.TwoDMatrix[0])
+            if x < 0 or x >= width: 
+                return True
+            if y < 0 or y >= height:
+                return True
+            return False        
+
         x1 = float(x1) 
         x2 = float(x2)     
         y1 = float(y1)  
-        y2 = float(y2)  
-        slope = (y2 - y1)/(x2 - x1)
-        for currentX in range(int(x1), int(x2)):
-            currentY = int(y1 + slope * (currentX - x1))
-            for offset in range(-self.botRadius+1, self.botRadius+1):
-                if  self.TwoDMatrix[currentY][currentX] > 200: 
-                    return True
+        y2 = float(y2)
+        if x1==x2 and y1==y2:
+            return False
+
+        if x1 == x2:     
+            slopeXY = (x2 - x1)/(y2 - y1)            
+            for currentY in range(int(y1), int(y2)):
+                currentX = int(x1 + slopeXY * (currentY - y1))
+                for offset in range(-self.botRadius+1, self.botRadius+1):
+                    if outOfIndex(currentX+offset, currentY):
+                        pass 
+                    elif  self.TwoDMatrix[currentY][currentX+offset] > 200: 
+                        return True  
+                    else:
+                        pass
+        elif y1 == y2: 
+            slopeYX = (y2 - y1)/(x2 - x1)
+            for currentX in range(int(x1), int(x2)):
+                currentY = int(y1 + slopeYX * (currentX - x1))
+                for offset in range(-self.botRadius+1, self.botRadius+1):
+                    if outOfIndex(currentX, currentY+offset): 
+                        pass
+                    elif  self.TwoDMatrix[currentY+offset][currentX] > 200: 
+                        return True
+                    else:
+                        pass    
+        else:    
+            slopeYX = (y2 - y1)/(x2 - x1)
+            for currentX in range(int(x1), int(x2)):
+                currentY = int(y1 + slopeYX * (currentX - x1))
+                for offset in range(-self.botRadius+1, self.botRadius+1):
+                    if outOfIndex(currentX, currentY+offset): 
+                        pass
+                    elif  self.TwoDMatrix[currentY+offset][currentX] > 200: 
+                        return True
+                    else:
+                        pass 
+            slopeXY = (x2 - x1)/(y2 - y1)            
+            for currentY in range(int(y1), int(y2)):
+                currentX = int(x1 + slopeXY * (currentY - y1))
+                for offset in range(-self.botRadius+1, self.botRadius+1):
+                    if outOfIndex(currentX+offset, currentY):
+                        pass 
+                    elif  self.TwoDMatrix[currentY][currentX+offset] > 200: 
+                        return True  
+                    else:
+                        pass         
 
         return False                 
 
